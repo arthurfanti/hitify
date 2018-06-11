6774234d27d5007baf1031534dfebcbad0142da8
Arthur Fanti <arthur@skore.io>
2018-06-11 01:07:24 -0300

    persist

--- client/index.js
+++ client/index.js
@@ -1,14 +1,15 @@
 import "babel-polyfill"
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { css } from 'glamor';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import { loadState, saveState } from './localStorage'
+import thunk from 'redux-thunk'
+import { css } from 'glamor'
 import rootReducer from './reducers'
-import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './containers/App'
+import registerServiceWorker from './registerServiceWorker'
 
 css.global('html', {
   margin: 0,
@@ -30,13 +31,19 @@ css.global('*', {
   boxSizing: 'border-box'
 })
 
+const persistedState = loadState()
 const store = createStore(
   rootReducer,
+  persistedState,
   applyMiddleware(thunk)
-);
+)
+
+store.subscribe(() => {
+  saveState(store.getState())
+})
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
-  </Provider>, document.getElementById('app'));
-registerServiceWorker();
+  </Provider>, document.getElementById('app'))
+registerServiceWorker()
